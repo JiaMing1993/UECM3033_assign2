7b0b0ffbc7c7063fd760ac5a12adcc5166505979
JiaMing1993 <jiamin.1993@1utar.edu.my>
2016-03-10 22:11:03 +0800

    Added task1.py

--- task1.py
+++ task1.py
@@ -1,29 +1,38 @@
 import numpy as np
-#Your optional code here
-#You can import some modules or create additional functions
+import scipy.linalg as sp
 
 def lu(A, b):
-    sol = []
-    # Edit here to implement your code
+    x = sp.lu_factor(A,False,True)
+    sol = sp.lu_solve(x,b,0,True)
     return list(sol)
 
 def sor(A, b):
-    sol = []
-    # Edit here to implement your code
+    L= -1*sp.tril(A,-1)
+    U=-1*sp.triu(A,1)
+    D=np.diag(np.diag(A))
+    Kj=np.linalg.inv(D).dot(L+U)
+    Y=np.max(np.abs(sp.eigvals(Kj)))
+    omega=(2-(1-(np.sqrt(1-Y**2))))/(Y**2)
+    Q = D/omega -L
+    K = np.linalg.inv(Q).dot(Q-A)
+    c = np.linalg.inv(Q).dot(b)
+    x = np.zeros_like(b)
+    for itr in range(10):
+        x    = K.dot(x) + c
+    sol=x
     return list(sol)
 
-def solve(A, b):
-    condition = True # State and implement your condition here
-    if condition:
-        print('Solve by lu(A,b)')
+
+def solve(A, b): 
+    try:       
+        np.linalg.cholesky(A)
+    except np.linalg.linalg.LinAlgError:
+        print('Solve by LU')
         return lu(A,b)
-    else:
-        print('Solve by sor(A,b)')
-        return sor(A,b)
+    print('Solve by SOR')    
+    return sor(A,b)
 
 if __name__ == "__main__":
-    ## import checker
-    ## checker.test(lu, sor, solve)
 
     A = [[2,1,6], [8,3,2], [1,5,1]]
     b = [9, 13, 7]
@@ -39,3 +48,5 @@ if __name__ == "__main__":
     b = [ 17603,  -63286,   56563,  -26523.5, 103396.5, -27906]
     sol = solve(A,b)
     print(sol)
+    
+    
--- task2.py
+++ task2.py
@@ -1,10 +1,35 @@
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import numpy as np
+import scipy.linalg as sp
 
-img=mpimg.imread('Lenna.tiff')
+img=mpimg.imread('pic.jpg')
 [r,g,b] = [img[:,:,i] for i in range(3)]
 
+Ur,Sr,Vr=sp.svd(r,full_matrices=False)
+Sr = np.diag(Sr)
+
+Ug,Sg,Vg=sp.svd(g,full_matrices=False)
+Sg = np.diag(Sg)
+
+Ub,Sb,Vb=sp.svd(b,full_matrices=False)
+Sb = np.diag(Sb)
+
+SrNew = np.zeros_like(Sr)
+SrNew[0:30] = Sr[0:30]
+Rnew = Ur.dot(SrNew).dot(Vr)
+
+SgNew = np.zeros_like(Sg)
+SgNew[0:30] = Sg[0:30]
+Gnew = Ug.dot(SgNew).dot(Vg)
+
+SbNew = np.zeros_like(Sb)
+SbNew[0:30] = Sb[0:30]
+Bnew = Ub.dot(SbNew).dot(Vb)
+
+img[:,:,0]=Rnew
+img[:,:,1]=Gnew
+img[:,:,2]=Bnew
 
 fig = plt.figure(1)
 ax1 = fig.add_subplot(2,2,1)
@@ -17,3 +42,43 @@ ax3.imshow(g, cmap = 'Greens')
 ax4.imshow(b, cmap = 'Blues')
 plt.show()
 
+#=========================================================================#
+
+img_2=mpimg.imread('pic.jpg')
+[r2,g2,b2] = [img_2[:,:,i] for i in range(3)]
+
+Ur2,Sr2,Vr2=sp.svd(r2,full_matrices=False)
+Sr2 = np.diag(Sr2)
+
+Ug2,Sg2,Vg2=sp.svd(g2,full_matrices=False)
+Sg2 = np.diag(Sg2)
+
+Ub2,Sb2,Vb2=sp.svd(b2,full_matrices=False)
+Sb2 = np.diag(Sb2)
+
+SrNew2 = np.zeros_like(Sr2)
+SrNew2[0:200] = Sr2[0:200]
+Rnew2 = Ur2.dot(SrNew2).dot(Vr2)
+
+SgNew2 = np.zeros_like(Sg2)
+SgNew2[0:200] = Sg2[0:200]
+Gnew2 = Ug2.dot(SgNew2).dot(Vg2)
+
+SbNew2 = np.zeros_like(Sb2)
+SbNew2[0:200] = Sb2[0:200]
+Bnew2 = Ub2.dot(SbNew2).dot(Vb2)
+
+img_2[:,:,0]=Rnew2
+img_2[:,:,1]=Gnew2
+img_2[:,:,2]=Bnew2
+
+fig = plt.figure(1)
+ax1 = fig.add_subplot(2,2,1)
+ax2 = fig.add_subplot(2,2,2)
+ax3 = fig.add_subplot(2,2,3)
+ax4 = fig.add_subplot(2,2,4)
+ax1.imshow(img_2)
+ax2.imshow(r2, cmap = 'Reds')
+ax3.imshow(g2, cmap = 'Greens')
+ax4.imshow(b2, cmap = 'Blues')
+plt.show()
